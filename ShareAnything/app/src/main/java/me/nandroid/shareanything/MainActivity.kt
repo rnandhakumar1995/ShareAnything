b30e8131994c9dae06121bf186449ad808383b94
nandha-12934 <nandha.ramasamy@zohocorp.com>
2022-10-02 21:56:21 +0530

    feat: Basic sending text to the client done

--- ShareAnything/app/src/main/java/me/nandroid/shareanything/MainActivity.kt
+++ ShareAnything/app/src/main/java/me/nandroid/shareanything/MainActivity.kt
@@ -14,7 +14,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import me.nandroid.shareanything.ui.theme.ShareAnythingTheme
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.*
 
 
 class MainActivity : ComponentActivity() {
@@ -31,27 +37,71 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    sealed class ServerStatus(var statusMessage: String) {
+        data class Running(val msg: String) : ServerStatus(msg)
+        data class Stopped(val msg: String) : ServerStatus(msg)
+        data class Error(val msg: String) : ServerStatus(msg)
+    }
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun Greeting() {
         Column {
-            var content by remember { mutableStateOf("Hello") }
-            var serverStatus by remember { mutableStateOf("Start Server") }
             TextField(modifier = Modifier.fillMaxWidth(), onValueChange = {
-                content = it
-            }, value = content)
+                viewModel.content = it
+            }, value = viewModel.content)
             Button(
                 modifier = Modifier
                     .fillMaxWidth(0.5f)
                     .padding(vertical = 20.dp)
                     .align(Alignment.CenterHorizontally),
                 onClick = {
-                    serverStatus = "Server running..."
-                    viewModel.startServer(content)
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        try {
+                            val b = viewModel.currentStatus !is ServerStatus.Running
+                            if (b) {
+                                val wifiIPAddress = getIPAddress(true)
+                                viewModel.currentStatus = ServerStatus.Running("Server running at $wifiIPAddress")
+                                viewModel.startServer()
+                            } else {
+                                viewModel.stopServer()
+                                viewModel.currentStatus = ServerStatus.Stopped("Start Server")
+                            }
+                        } catch (e: Exception) {
+                            viewModel.currentStatus = ServerStatus.Error(e.message.toString())
+                        }
+                    }
                 }) {
-                Text(text = serverStatus)
+                Text(text = viewModel.currentStatus.statusMessage)
             }
         }
+    }
+
+    private fun getIPAddress(useIPv4: Boolean): String? {
+        try {
+            val interfaces: List<NetworkInterface> = Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (networkInterface in interfaces) {
+                val addresses: List<InetAddress> = Collections.list(networkInterface.inetAddresses)
+                for (address in addresses) {
+                    if (!address.isLoopbackAddress) {
+                        val hostAddress = address.hostAddress
+                        val isIPv4 = (hostAddress?.indexOf(':') ?: return null) < 0
+                        if (useIPv4) {
+                            if (isIPv4) return hostAddress
+                        } else {
+                            if (!isIPv4) {
+                                val delimiter = hostAddress.indexOf('%') // drop ip6 zone suffix
+                                return if (delimiter < 0) hostAddress.uppercase(Locale.getDefault()) else hostAddress.substring(0, delimiter)
+                                    .uppercase(Locale.getDefault())
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        } // for now eat exceptions
+        return ""
     }
 
     @Preview(showBackground = true)
--- ShareAnything/app/src/main/java/me/nandroid/shareanything/MainActivityViewModel.kt
+++ ShareAnything/app/src/main/java/me/nandroid/shareanything/MainActivityViewModel.kt
@@ -1,29 +1,31 @@
 package me.nandroid.shareanything
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class MainActivityViewModel: ViewModel() {
-    fun startServer(content: String){
-        viewModelScope.launch(context = Dispatchers.IO) {
-            embeddedServer(Netty, port = 8080) {
-                routing {
-                    get("/") {
-                        call.respondText(content ?: "No content found")
-                    }
+class MainActivityViewModel : ViewModel() {
+    var content by mutableStateOf("")
+    var currentStatus by mutableStateOf<MainActivity.ServerStatus>(MainActivity.ServerStatus.Stopped("Start Server"))
+    private var embeddedServer: NettyApplicationEngine? = null
+    suspend fun startServer() {
+        embeddedServer = embeddedServer(Netty, port = 8080) {
+            routing {
+                get("/") {
+                    call.respondText(content)
                 }
-            }.start(wait = true)
+            }
         }
+        embeddedServer?.start(wait = true)
     }
 
-    fun onTextChange() {
-
+    suspend fun stopServer() {
+        embeddedServer?.stop()
     }
 }
